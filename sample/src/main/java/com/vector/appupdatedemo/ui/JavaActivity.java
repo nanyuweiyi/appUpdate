bfd8965ad5506f867a51442c581254a4fc6d5930
tiannannan <tiannannan@nutfin.cn>
2018-01-22 15:00:07 +0800

    init

---
+++ sample/src/main/java/com/vector/appupdatedemo/ui/JavaActivity.java
@@ -0,0 +1,494 @@
+package com.vector.appupdatedemo.ui;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.vector.appupdatedemo.R;
+import com.vector.appupdatedemo.http.OkGoUpdateHttpUtil;
+import com.vector.appupdatedemo.http.UpdateAppHttpUtil;
+import com.vector.appupdatedemo.util.CProgressDialogUtils;
+import com.vector.appupdatedemo.util.HProgressDialogUtils;
+import com.vector.update_app.SilenceUpdateCallback;
+import com.vector.update_app.UpdateAppBean;
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.UpdateCallback;
+import com.vector.update_app.service.DownloadService;
+import com.vector.update_app.utils.AppUpdateUtils;
+import com.vector.update_app.utils.DrawableUtil;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JavaActivity extends AppCompatActivity {
+    private String mUpdateUrl = "https://raw.githubusercontent.com/WVector/AppUpdateDemo/master/json/json.txt";
+    private boolean isShowDownloadProgress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java);
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_diy_1));
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_diy_2));
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_diy_3));
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_default_silence));
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_default_silence_diy_dialog));
+        DrawableUtil.setTextStrokeTheme((Button) findViewById(R.id.btn_default), 0xffe94339);
+
+
+    }
+
+    /**
+     * 最简方式
+     *
+     * @param view
+     */
+    public void updateApp(View view) {
+        new UpdateAppManager
+                .Builder()
+                //当前Activity
+                .setActivity(this)
+                //更新地址
+                .setUpdateUrl(mUpdateUrl)
+                //实现httpManager接口的对象
+                .setHttpManager(new UpdateAppHttpUtil())
+                .build()
+                .update();
+    }
+
+    /**
+     * 自定义接口协议
+     *
+     * @param view
+     */
+    public void updateDiy(View view) {
+
+//        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("appKey", "ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f");
+        params.put("appVersion", AppUpdateUtils.getVersionName(this));
+        params.put("key1", "value2");
+        params.put("key2", "value3");
+
+
+        new UpdateAppManager
+                .Builder()
+                //必须设置，当前Activity
+                .setActivity(this)
+                //必须设置，实现httpManager接口的对象
+                .setHttpManager(new OkGoUpdateHttpUtil())
+                //必须设置，更新地址
+                .setUpdateUrl(mUpdateUrl)
+
+                //以下设置，都是可选
+                //设置请求方式，默认get
+                .setPost(false)
+                //不显示通知栏进度条
+//                .dismissNotificationProgress()
+                //是否忽略版本
+//                .showIgnoreVersion()
+                //添加自定义参数，默认version=1.0.0（app的versionName）；apkKey=唯一表示（在AndroidManifest.xml配置）
+                .setParams(params)
+                //设置点击升级后，消失对话框，默认点击升级后，对话框显示下载进度
+                .hideDialogOnDownloading(false)
+                //设置头部，不设置显示默认的图片，设置图片后自动识别主色调，然后为按钮，进度条设置颜色
+                .setTopPic(R.mipmap.top_8)
+                //为按钮，进度条设置颜色。
+                .setThemeColor(0xffffac5d)
+                //设置apk下砸路径，默认是在下载到sd卡下/Download/1.0.0/test.apk
+//                .setTargetPath(path)
+                //设置appKey，默认从AndroidManifest.xml获取，如果，使用自定义参数，则此项无效
+//                .setAppKey("ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f")
+
+                .build()
+                //检测是否有新版本
+                .checkNewApp(new UpdateCallback() {
+                    /**
+                     * 解析json,自定义协议
+                     *
+                     * @param json 服务器返回的json
+                     * @return UpdateAppBean
+                     */
+                    @Override
+                    protected UpdateAppBean parseJson(String json) {
+                        UpdateAppBean updateAppBean = new UpdateAppBean();
+                        try {
+                            JSONObject jsonObject = new JSONObject(json);
+                            updateAppBean
+                                    //（必须）是否更新Yes,No
+                                    .setUpdate(jsonObject.optString("update"))
+                                    //（必须）新版本号，
+                                    .setNewVersion(jsonObject.optString("new_version"))
+                                    //（必须）下载地址
+                                    .setApkFileUrl(jsonObject.optString("apk_file_url"))
+                                    //测试下载路径是重定向路径
+//                                    .setApkFileUrl("http://openbox.mobilem.360.cn/index/d/sid/3282847")
+                                    //（必须）更新内容
+//                                    .setUpdateLog(jsonObject.optString("update_log"))
+                                    //测试内容过度
+//                                    .setUpdateLog("测试")
+                                    .setUpdateLog("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16")
+//                                    .setUpdateLog("今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说相对于其他行业来说今天我们来聊一聊程序员枯燥的编程生活，相对于其他行业来说\r\n")
+                                    //大小，不设置不显示大小，可以不设置
+                                    .setTargetSize(jsonObject.optString("target_size"))
+                                    //是否强制更新，可以不设置
+                                    .setConstraint(false)
+                                    //设置md5，可以不设置
+                                    .setNewMd5(jsonObject.optString("new_md51"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        return updateAppBean;
+                    }
+
+                    @Override
+                    protected void hasNewApp(UpdateAppBean updateApp, UpdateAppManager updateAppManager) {
+                        updateAppManager.showDialogFragment();
+                    }
+
+                    /**
+                     * 网络请求之前
+                     */
+                    @Override
+                    public void onBefore() {
+                        CProgressDialogUtils.showProgressDialog(JavaActivity.this);
+                    }
+
+                    /**
+                     * 网路请求之后
+                     */
+                    @Override
+                    public void onAfter() {
+                        CProgressDialogUtils.cancelProgressDialog(JavaActivity.this);
+                    }
+
+                    /**
+                     * 没有新版本
+                     */
+                    @Override
+                    public void noNewApp() {
+                        Toast.makeText(JavaActivity.this, "没有新版本", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    /**
+     * 自定义对话框
+     *
+     * @param updateApp
+     * @param updateAppManager
+     */
+    private void showDiyDialog(final UpdateAppBean updateApp, final UpdateAppManager updateAppManager) {
+        String targetSize = updateApp.getTargetSize();
+        String updateLog = updateApp.getUpdateLog();
+
+        String msg = "";
+
+        if (!TextUtils.isEmpty(targetSize)) {
+            msg = "新版本大小：" + targetSize + "\n\n";
+        }
+
+        if (!TextUtils.isEmpty(updateLog)) {
+            msg += updateLog;
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(String.format("是否升级到%s版本？", updateApp.getNewVersion()))
+                .setMessage(msg)
+                .setPositiveButton("升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //显示下载进度
+                        if (isShowDownloadProgress) {
+                            updateAppManager.download(new DownloadService.DownloadCallback() {
+                                @Override
+                                public void onStart() {
+                                    HProgressDialogUtils.showHorizontalProgressDialog(JavaActivity.this, "下载进度", false);
+                                }
+
+                                /**
+                                 * 进度
+                                 *
+                                 * @param progress  进度 0.00 -1.00 ，总大小
+                                 * @param totalSize 总大小 单位B
+                                 */
+                                @Override
+                                public void onProgress(float progress, long totalSize) {
+                                    HProgressDialogUtils.setProgress(Math.round(progress * 100));
+                                }
+
+                                /**
+                                 *
+                                 * @param total 总大小 单位B
+                                 */
+                                @Override
+                                public void setMax(long total) {
+
+                                }
+
+
+                                @Override
+                                public boolean onFinish(File file) {
+                                    HProgressDialogUtils.cancel();
+                                    return true;
+                                }
+
+                                @Override
+                                public void onError(String msg) {
+                                    Toast.makeText(JavaActivity.this, msg, Toast.LENGTH_SHORT).show();
+                                    HProgressDialogUtils.cancel();
+
+                                }
+                            });
+                        } else {
+                            //不显示下载进度
+                            updateAppManager.download();
+                        }
+
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("暂不升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    /**
+     * 自定义接口协议+自定义对话框
+     *
+     * @param view
+     */
+    public void updateDiy2(View view) {
+        //不显示下载进度
+        isShowDownloadProgress = false;
+        diyUpdate();
+
+    }
+
+    private void diyUpdate() {
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        Map<String, String> params = new HashMap<String, String>();
+
+        params.put("appKey", "ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f");
+        params.put("appVersion", AppUpdateUtils.getVersionName(this));
+        params.put("key1", "value2");
+        params.put("key2", "value3");
+
+        new UpdateAppManager
+                .Builder()
+                //必须设置，当前Activity
+                .setActivity(this)
+                //必须设置，实现httpManager接口的对象
+                .setHttpManager(new OkGoUpdateHttpUtil())
+                //必须设置，更新地址
+                .setUpdateUrl(mUpdateUrl)
+
+                //以下设置，都是可选
+                //设置请求方式，默认get
+                .setPost(false)
+                //添加自定义参数，默认version=1.0.0（app的versionName）；apkKey=唯一表示（在AndroidManifest.xml配置）
+                .setParams(params)
+                //设置apk下砸路径，默认是在下载到sd卡下/Download/1.0.0/test.apk
+                .setTargetPath(path)
+                //设置appKey，默认从AndroidManifest.xml获取，如果，使用自定义参数，则此项无效
+//                .setAppKey("ab55ce55Ac4bcP408cPb8c1Aaeac179c5f6f")
+
+                .build()
+                //检测是否有新版本
+                .checkNewApp(new UpdateCallback() {
+                    /**
+                     * 解析json,自定义协议
+                     *
+                     * @param json 服务器返回的json
+                     * @return UpdateAppBean
+                     */
+                    @Override
+                    protected UpdateAppBean parseJson(String json) {
+                        UpdateAppBean updateAppBean = new UpdateAppBean();
+                        try {
+                            JSONObject jsonObject = new JSONObject(json);
+                            updateAppBean
+                                    //（必须）是否更新Yes,No
+                                    .setUpdate(jsonObject.optString("update"))
+                                    //（必须）新版本号，
+                                    .setNewVersion(jsonObject.optString("new_version"))
+                                    //（必须）下载地址
+                                    .setApkFileUrl(jsonObject.optString("apk_file_url"))
+
+                                    //（必须）更新内容
+                                    .setUpdateLog(jsonObject.optString("update_log"))
+
+                                    //大小，不设置不显示大小，可以不设置
+                                    .setTargetSize(jsonObject.optString("target_size"))
+                                    //是否强制更新，可以不设置
+                                    .setConstraint(false)
+                                    //设置md5，可以不设置
+                                    .setNewMd5(jsonObject.optString("new_md51"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        return updateAppBean;
+                    }
+
+                    /**
+                     * 有新版本
+                     *
+                     * @param updateApp        新版本信息
+                     * @param updateAppManager app更新管理器
+                     */
+                    @Override
+                    public void hasNewApp(UpdateAppBean updateApp, UpdateAppManager updateAppManager) {
+                        //强制更新，
+                        if (updateApp.isConstraint()) {
+
+                        } else {
+
+                        }
+                        //自定义对话框
+                        showDiyDialog(updateApp, updateAppManager);
+                    }
+
+                    /**
+                     * 网络请求之前
+                     */
+                    @Override
+                    public void onBefore() {
+                        CProgressDialogUtils.showProgressDialog(JavaActivity.this);
+                    }
+
+                    /**
+                     * 网路请求之后
+                     */
+                    @Override
+                    public void onAfter() {
+                        CProgressDialogUtils.cancelProgressDialog(JavaActivity.this);
+                    }
+
+                    /**
+                     * 没有新版本
+                     */
+                    @Override
+                    public void noNewApp() {
+                        Toast.makeText(JavaActivity.this, "没有新版本", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    /**
+     * 自定义接口协议+自定义对话框+显示进度对话框
+     *
+     * @param view
+     */
+    public void updateDiy3(View view) {
+//        显示下载进度
+        isShowDownloadProgress = true;
+        diyUpdate();
+    }
+
+    /**
+     * 静默下载，下载完才弹出升级界面
+     *
+     * @param view
+     */
+    public void silenceUpdateApp(View view) {
+        new UpdateAppManager
+                .Builder()
+                //当前Activity
+                .setActivity(this)
+                //更新地址
+                .setUpdateUrl(mUpdateUrl)
+                //实现httpManager接口的对象
+                .setHttpManager(new UpdateAppHttpUtil())
+                //只有wifi下进行，静默下载(只对静默下载有效)
+                .setOnlyWifi()
+                .build()
+                .silenceUpdate();
+    }
+
+    /**
+     * 静默下载，并且自定义对话框
+     *
+     * @param view
+     */
+    public void silenceUpdateAppAndDiyDialog(View view) {
+        new UpdateAppManager
+                .Builder()
+                //当前Activity
+                .setActivity(this)
+                //更新地址
+                .setUpdateUrl(mUpdateUrl)
+                //实现httpManager接口的对象
+                .setHttpManager(new UpdateAppHttpUtil())
+                //只有wifi下进行，静默下载(只对静默下载有效)
+                .setOnlyWifi()
+                .build()
+                .checkNewApp(new SilenceUpdateCallback() {
+                    @Override
+                    protected void showDialog(UpdateAppBean updateApp, UpdateAppManager updateAppManager, File appFile) {
+                        showSilenceDiyDialog(updateApp, appFile);
+                    }
+                });
+    }
+
+    /**
+     * 静默下载自定义对话框
+     *
+     * @param updateApp
+     * @param appFile
+     */
+    private void showSilenceDiyDialog(final UpdateAppBean updateApp, final File appFile) {
+        String targetSize = updateApp.getTargetSize();
+        String updateLog = updateApp.getUpdateLog();
+
+        String msg = "";
+
+        if (!TextUtils.isEmpty(targetSize)) {
+            msg = "新版本大小：" + targetSize + "\n\n";
+        }
+
+        if (!TextUtils.isEmpty(updateLog)) {
+            msg += updateLog;
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(String.format("是否升级到%s版本？", updateApp.getNewVersion()))
+                .setMessage(msg)
+                .setPositiveButton("安装", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        AppUpdateUtils.installApp(JavaActivity.this, appFile);
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("暂不升级", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+}
